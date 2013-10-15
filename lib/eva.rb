f11109789e0b5ff1e100c2f6065e42259bceaa72
kkxlkkxllb <kkxlkkxllb@gmail.com>
2013-10-15 20:05:02 +0800

    tmp

--- lib/eva.rb
+++ lib/eva.rb
@@ -59,7 +59,7 @@ module Eva
 
 	class Icard < Base
 		def list(num)
-			Word.limit(num).as_json.group_by{|x| x['title'][0,1].downcase }
+			Word.limit(num).as_json#.group_by{|x| x['title'][0,1].downcase }
 		end
 	end
 
