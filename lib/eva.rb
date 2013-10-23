24f2269300096692cdc88e4a73f545a74c98699c
libiao <kkxlkkxllb@gmail.com>
2013-10-23 10:13:22 +0800

    fix

--- lib/eva.rb
+++ lib/eva.rb
@@ -59,8 +59,9 @@ module Eva
 
 	class Icard < Base
 		def list(num)
+			quotes = Quote.lt(100)
 			Word.limit(num).map do |w|
-				q = Quote.lt(100).content_by(w.title).limit(3).as_json(only: [:content])
+				q = quotes.content_by(w.title).limit(3).as_json(only: [:content])
 				w.as_json.merge!(quotes: q)
 			end
 			#.group_by{|x| x['title'][0,1].downcase }
